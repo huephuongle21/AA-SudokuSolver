ffa3c424e73e35ff392d2bcffae11e9c37de9181
Hue Phuong Le s3687477 <s3687477@student.rmit.edu.au>
2020-06-03 15:13:41 +1000

    done algorX

--- src/solver/AlgorXSolver.java
+++ src/solver/AlgorXSolver.java
@@ -17,7 +17,8 @@ public class AlgorXSolver extends StdSudokuSolver
 	private ArrayList<Integer> tmpSolution = new ArrayList<Integer>();
 	private ArrayList<Integer> solution;
 	private int[][] backtracking;
-	private ArrayList<Integer> backtrackList = new ArrayList<Integer>();
+	private ArrayList<Integer> removedCols = new ArrayList<Integer>();
+	private ArrayList<Integer> listRows = new ArrayList<Integer>();
 	
     public AlgorXSolver() {
         // TODO: any initialisation you want to implement.
@@ -33,28 +34,81 @@ public class AlgorXSolver extends StdSudokuSolver
     	backtracking = new int[coverRows][coverCols];
     	    	    	
     	// Sub-method to solve sudoku using matrix
-    	process(0);
+    	process();
     	if(solution != null) {
     		transformToSudoku(grid);
+    		return true;
     	}
-
         // placeholder
         return false;
     } // end of solve()
     
     private void transformToSudoku(SudokuGrid grid) {
-    	for(int i = 0; i != solution.size(); i++) {
-    		
+    	for(int i = 0; i != solution.size(); i++) {    		
     		int index = solution.get(i);
-    		System.out.println(index);
-    		int value = (index%size) + 1;
-    		int col = ((index-value+1)/size)%size;
-    		int row = (index - (value-1) - col*size)/size/size;
+    		int valueIndex = (index%size) + 1;
+    		int col = ((index-valueIndex+1)/size)%size;
+    		int row = (index - (valueIndex-1) - col*size)/size/size;
+    		int value = valueRange[valueIndex-1];
     		grid.setValue(row, col, value);    		
     	}
     }
+
+    private void process() {
+    	boolean finished = false;
+    	while(!finished) {
+    		int col = chooseCol();
+    		removedCols.add(col);
+    		chooseRow(col);
+    		boolean valid = false;
+    		int i = 0;
+    		while(!valid) {
+    			int row = listRows.get(i);
+    			cover(row);
+    			if(isEmpty()) {
+    				valid = true;
+    				finished = true;
+    			} else {
+    				if(!isRowValid()) {
+    					uncover(row);
+    					i++;
+    				} else {
+    					listRows.clear();
+    					valid = true;
+    				}
+    			}
+    		}
+    	}
+    }
     
-    private void process(int k) {   	
+    private void chooseRow(int col) {
+    	for(int i = 0; i < coverRows; i++) {
+    		if(matrix[i][col] == 1) {
+    			listRows.add(i);
+    		}
+    	}
+    }
+
+    private boolean isRowValid() {
+    	int count = 0;
+    	for(int i = 0; i != coverCols; i++) {
+    		if(!isRemoved(i)) {
+    			for(int j = 0; j != coverRows; j++) {
+    				if(matrix[j][i] == 1) {
+    					count++;
+    				}
+    			}
+    			if(count == 0) {
+    				return false;
+    			} else {
+    				count = 0;
+    			}
+    		}
+    	}
+    	return true;
+    }
+
+    private boolean isEmpty() {
     	int count = 0;
     	for(int i = 0; i != coverRows; i++) {
     		for(int j = 0; j != coverCols; j++) {
@@ -65,120 +119,121 @@ public class AlgorXSolver extends StdSudokuSolver
     	}
     	if(count == 0) {
     		solution = new ArrayList<Integer>(tmpSolution);
+    		return true;
     	} else {
-			int col = chooseCol();
-			if(col != -1) {
-				chooseRow(col);
-				process(k+1);
-			} else {
-				backtrack();
-				process(k+1);
-			}
+    		return false;
     	}
     }
-    
-//    private int findCol() {
-//    	int returnCol = -1;
-//    	int count = 0;
-//    	for(int i = 0; i != coverCols; i++) {
-//    		for(int j = 0; j != coverRows; j++) {
-//    			if(alrBacktrack(j) && matrix[j][i] == 1) {
-//    				returnCol = i;
-//    				count++;
-//    				if(count == NUMBER_CONSTRAINTS) {
-//    					return returnCol;
-//    				}
-//    			}
-//    		}
-//    	}
-//    	return -1;
-//    }
-    
-    private void chooseRow(int col) {
-    	for(int i = 0; i != coverRows; i++) {
-    		if(matrix[i][col] == 1) {
-    			tmpSolution.add(i);
-    			for(int j = 0; j != coverCols; j++) {
-    				if(matrix[i][j] == 1) {
-    					matrix[i][j] = 0;
-    					backtracking[i][j] = 1;
-    					for(int k = 0; k != coverRows; k++) {
-        					if(matrix[k][j] == 1) {
-        						matrix[k][j] = 0;
-        						backtracking[k][j] = 1;
-        					}
-        				}
-    				}
+
+    private void cover(int row) {
+    	tmpSolution.add(row);
+    	ArrayList<Integer> listCols = new ArrayList<Integer>();
+    	for(int i = 0; i != coverCols; i++) {
+    		if(matrix[row][i] == 1) {
+    			listCols.add(i);
+    			removedCols.add(i);
+    		}
+    	}
+    	ArrayList<Integer> listRows = new ArrayList<Integer>();
+    	for(int i = 0; i != listCols.size(); i++) {
+    		int col = listCols.get(i);
+    		for(int j = 0; j != coverRows; j++) {
+    			if(matrix[j][col] == 1) {
+    				listRows.add(j);
     			}
-    		}    		
-    	}
-    }
-    
-    private void backtrack() {
-    	if(tmpSolution.size() != 0) {
-    		int i = tmpSolution.size()-1;
-    		int index = tmpSolution.get(i);
-    		tmpSolution.remove(i);
-    		backtrackList.add(index);
-    		
-    		for(int j = 0; j != coverCols; j++) {
-    			if(backtracking[index][j] == 1) {
-    				backtracking[index][j] = 0;
-    				matrix[index][j] = 1;
-    				for(int k = 0; k != coverRows; k++) {
-    					if(backtracking[k][j] == 1) {
-    						index = k;
-    						backtracking[k][j] = 0;
-    						matrix[k][j] = 1;
-    						for(int l = 0; l != coverCols; l++) {
-    							if(backtracking[k][l] == 1) {
-    								backtracking[k][l] = 0;
-    								matrix[k][l] = 1;
-    							}
-    						}
-    					}
-    					if(index == k) {
-    						for(int row = 0; row != tmpSolution.size(); row++) {
-    							if(tmpSolution.get(row) == index) {
-    								tmpSolution.remove(row);
-    								backtrackList.add(index);
-    							}
-    						}
-    					}
+    		}
+    		for(int k = 0; k != listRows.size(); k++) {
+    			int subRow = listRows.get(k);
+    			for(int a = 0; a != coverCols; a++) {
+    				if(matrix[subRow][a] == 1) {
+    					matrix[subRow][a] = 0;
+    					backtracking[subRow][a] = 1;
     				}
     			}
     		}
+    		listRows.clear();
+    	}
+    }
+    
+    private void undoCols(int value) {
+    	int index = -1;
+    	for(int i = 0; i != removedCols.size(); i++) {
+    		if(removedCols.get(i) == value) {
+    			index = value;
+    		}
+    	}
+    	if(index != -1) {
+    		removedCols.remove(index);
+    	}
+    }
+    
+    private boolean isRemoved(int value) {
+    	boolean removed = false;
+    	for(int i = 0; i != removedCols.size(); i++) {
+    		if(removedCols.get(i) == value) {
+    			removed = true;
+    		}
+    	}
+    	return removed;
+    }
+
+    private void uncover(int row) {
+    	int index = -1;
+    	for(int i = 0; i != tmpSolution.size(); i++) {
+    		if(tmpSolution.get(i) == row) {
+    			index = i;
+    		}
+    	}
+    	if(index != -1) {
+    		tmpSolution.remove(index);
+    	}
+    	ArrayList<Integer> listCols = new ArrayList<Integer>();
+    	for(int i = 0; i != coverCols; i++) {
+    		if(backtracking[row][i] == 1) {
+    			listCols.add(i);
+    			undoCols(i);
+    		}
+    	}
+    	ArrayList<Integer> listRows = new ArrayList<Integer>();
+    	for(int i = 0; i != listCols.size(); i++) {
+    		int col = listCols.get(i);
+    		for(int j = 0; j != coverRows; j++) {
+    			if(backtracking[j][col] == 1) {
+    				listRows.add(j);
+    			}
+    		}
+    		for(int k = 0; k != listRows.size(); k++) {
+    			int subRow = listRows.get(k);
+    			for(int a = 0; a != coverCols; a++) {
+    				if(backtracking[subRow][a] == 1) {
+    					backtracking[subRow][a] = 0;
+    					matrix[subRow][a] = 1;
+    				}
+    			}
+    		}
+    		listRows.clear();
     	}
     }
     
     private int chooseCol() {
-    	int size = coverRows;
+    	int min = Integer.MAX_VALUE;
     	int count = 0;
     	int col = -1;
     	for(int i = 0; i != coverCols; i++) {
     		for(int j = 0; j != coverRows; j++) {
-    			if(alrBacktrack(j) && matrix[j][i] == 1) {
+    			if(matrix[j][i] == 1) {
     				count++;
     			}
     		}
-    		if(count < size && count > 0) {
-    			size = count;
-    			count = 0;
-    			col = i;
-    		} 
-
+    		if (count > 0) {
+				if (count < min || col == -1) {
+    				min = count;
+    				col = i;
+    			}
+			}
+    		count = 0;
     	}
     	return col;
     }
-    
-    private boolean alrBacktrack(int index) {
-    	for(int i = 0; i != backtrackList.size(); i++) {
-    		if(backtrackList.get(i) == index) {
-    			return false;
-    		}
-    	}
-    	return true;
-    }
-    
 
 } // end of class AlgorXSolver
